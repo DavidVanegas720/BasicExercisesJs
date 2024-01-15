b76f29d66427c8c86a0a72b66156412369f0fd44
DavidVanegas720 <david_1069@hotmail.com>
2024-01-15 10:47:23 -0500

    ejercicios basicos de js

---
+++ jsExercises/app.js
@@ -0,0 +1,77 @@
+// exercise #1
+
+// function countCharacters (chain) {
+//     return chain.length;
+// }
+
+// let text = "This is an example of a function that counts the number of characters in a text string";
+
+// let numberOfCharacters =countCharacters(text);
+
+// console.log(`This text string has ${numberOfCharacters} characters`)
+
+
+
+
+
+
+// exercise #2
+
+    // function cropText (text, maxLenght) {
+    //     if (text.lenght <= maxLenght) {
+    //         return text;
+    //     } else{
+    //         return text.slice(0, maxLenght) + '...'
+    //     }
+    // }
+
+    // let originalText = "Esta es una cadena de texto que sera recortada, ya que supera el numero maximo de caracteres permitidos"
+    // let croppedText = cropText(originalText, 50);
+
+    // console.log("Texto original: " + originalText);
+    // console.log("Texto recortado: " + croppedText);
+
+
+
+
+
+    // exercise #3
+
+        // function separeByObject (string, separator) {
+        //     return string.split(separator);
+        // }
+
+        // let originalString = "Hola,esto,es,una,frase";
+        // let separator = ",";
+
+        // let arraySeparate = separeByObject(originalString,separator);
+
+        // console.log("cadena original " + originalString);
+        // console.log("array separado" + JSON.stringify(arraySeparate));
+
+
+
+
+
+
+        // exercise #4
+
+        // function repeatText (text, timeRepeat) {
+        //     let result = "";
+
+        //     for(let i = 0; i < timeRepeat; i++) {
+        //         result += text;
+        //     }
+
+        //     return result;
+        // }
+
+        // let originalText = "Hola ";
+        // let repetitions = 3;
+
+        // let repeatedText = repeatText(originalText, repetitions);
+
+        // console.log("Texto original: " + originalText);
+        // console.log("Texto repetido " + repeatedText);
+
+
